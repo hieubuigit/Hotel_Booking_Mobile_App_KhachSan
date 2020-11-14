d6202f974dc22e5e25df98b92ee3c717deaa7d0d
hieubuigit <hieubv1306@gmail.com>
2020-11-14 16:53:19 +0700

    Hoan thanh 50% giao dien man hinh Cap nhat thong tin phong

---
+++ app/src/main/java/com/chuyende/hotelbookingappofhotel/activities/CapNhatPhongActivity.java
@@ -0,0 +1,16 @@
+package com.chuyende.hotelbookingappofhotel.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chuyende.hotelbookingappofhotel.R;
+
+public class CapNhatPhongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cap_nhat_phong_layout);
+    }
+}
